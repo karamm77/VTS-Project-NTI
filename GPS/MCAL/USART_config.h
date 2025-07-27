f27f04c49069ec42eabefbefe1216c80ae7e1924
karamm77 <96154781+karamm77@users.noreply.github.com>
2025-07-27 12:53:23 +0200

    Update USART_config.h

--- GPS/MCAL/USART_config.h
+++ GPS/MCAL/USART_config.h
@@ -1,6 +1,6 @@
 /**
  * @file USART_config.h
- * @author Mohamed Dwedar (MohamedDwedar2699@gmail.com)
+ * @author Karam Albendary
  * @brief configuration of USART
  * @version 0.1
  * @date 2023-08-26
